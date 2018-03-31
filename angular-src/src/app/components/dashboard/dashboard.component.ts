a3f6bca952bfe5905634fca19282224f5e1c8be1
Xinglong <jia.xi@husky.neu.edu>
2018-03-31 11:31:57 -0400

    dashboard
    dashboard

---
+++ angular-src/src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+//used to display the dashboard
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
